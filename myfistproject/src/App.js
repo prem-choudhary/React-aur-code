b2f87c7bcd368650123a0022d7e54b26197f716e
Prem-choudhary <pk5480524@gmail.com>
2026-06-06 11:53:15 +0530

    react app without vite

---
+++ myfistproject/src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <>
+    <h1> Hello react user my name is one two three 😀 </h1>
+    <h1>Created without Vite.</h1>
+    </>
+    
+  );
+}
+
+export default App;
